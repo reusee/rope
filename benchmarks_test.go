76d7c565fcd97e85cfb749bd3994615965bc6f9f
reus <reusee@gmail.com>
2014-10-11 16:42:09 +0800

    add New and Len benchmarks

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -5,6 +5,20 @@ import (
 	"testing"
 )
 
+func BenchmarkNew(b *testing.B) {
+	bytes := make([]byte, 409600)
+	n, err := rand.Read(bytes)
+	if n != len(bytes) || err != nil {
+		b.Fatalf("%d %v", n, err)
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		b.SetBytes(int64(len(bytes)))
+		NewFromBytes(bytes)
+	}
+}
+
 func BenchmarkIndex(b *testing.B) {
 	bytes := make([]byte, 409600)
 	n, err := rand.Read(bytes)
@@ -16,6 +30,20 @@ func BenchmarkIndex(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		r.Index(300000)
+	}
+}
+
+func BenchmarkLen(b *testing.B) {
+	bytes := make([]byte, 409600)
+	n, err := rand.Read(bytes)
+	if n != len(bytes) || err != nil {
+		b.Fatalf("%d %v", n, err)
+	}
+	r := NewFromBytes(bytes)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		r.Len()
 	}
 }
 
