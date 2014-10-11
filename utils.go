2c5405759f6e8d74e6af08db1d75c4d7688924e8
reus <reusee@gmail.com>
2014-10-11 19:53:53 +0800

    fix Dump

--- utils.go
+++ utils.go
@@ -33,15 +33,15 @@ func (r *Rope) StructEqual(r2 *Rope) bool {
 }
 
 func (r *Rope) Dump() {
-	r.dump(0)
+	r.dump(0, "")
 }
 
-func (r *Rope) dump(level int) {
-	p("%s%d |%s|\n", strings.Repeat("  ", level), r.weight, r.content)
+func (r *Rope) dump(level int, prefix string) {
+	p("%s%s%d |%s|\n", strings.Repeat("  ", level), prefix, r.weight, r.content)
 	if r.left != nil {
-		r.left.dump(level + 1)
+		r.left.dump(level+1, "<")
 	}
 	if r.right != nil {
-		r.right.dump(level + 1)
+		r.right.dump(level+1, ">")
 	}
 }
