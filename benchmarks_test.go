0abc2633eda5d012d735728ba96f0269feb4728d
reus <reusee@gmail.com>
2014-10-12 10:25:12 +0800

    cleanup benchmarks

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -2,116 +2,102 @@ package rope
 
 import (
 	"crypto/rand"
+	"log"
 	"testing"
 )
 
-func BenchmarkNew(b *testing.B) {
-	bytes := make([]byte, 409600)
+const benchBytesLen = 409600
+
+func getBenchBytes() []byte {
+	bytes := make([]byte, benchBytesLen)
 	n, err := rand.Read(bytes)
 	if n != len(bytes) || err != nil {
-		b.Fatalf("%d %v", n, err)
+		log.Fatalf("%d %v", n, err)
 	}
-	b.ResetTimer()
+	return bytes
+}
 
+func getBenchRope() *Rope {
+	return NewFromBytes(getBenchBytes())
+}
+
+func BenchmarkNew(b *testing.B) {
+	bytes := getBenchBytes()
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		b.SetBytes(int64(len(bytes)))
+		b.SetBytes(benchBytesLen)
 		NewFromBytes(bytes)
 	}
 }
 
 func BenchmarkIndex(b *testing.B) {
-	bytes := make([]byte, 409600)
-	n, err := rand.Read(bytes)
-	if n != len(bytes) || err != nil {
-		b.Fatalf("%d %v", n, err)
-	}
-	r := NewFromBytes(bytes)
+	r := getBenchRope()
 	b.ResetTimer()
-
 	for i := 0; i < b.N; i++ {
 		r.Index(300000)
 	}
 }
 
 func BenchmarkLen(b *testing.B) {
-	bytes := make([]byte, 409600)
-	n, err := rand.Read(bytes)
-	if n != len(bytes) || err != nil {
-		b.Fatalf("%d %v", n, err)
-	}
-	r := NewFromBytes(bytes)
+	r := getBenchRope()
 	b.ResetTimer()
-
 	for i := 0; i < b.N; i++ {
 		r.Len()
 	}
 }
 
-func BenchmarkSplit(b *testing.B) {
-	bytes := make([]byte, 409600)
-	n, err := rand.Read(bytes)
-	if n != len(bytes) || err != nil {
-		b.Fatalf("%d %v", n, err)
-	}
-	r := NewFromBytes(bytes)
+func BenchmarkBytes(b *testing.B) {
+	r := getBenchRope()
 	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r.Bytes()
+	}
+}
 
+func BenchmarkConcat(b *testing.B) {
+	r1 := getBenchRope()
+	r2 := getBenchRope()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r1.Concat(r2)
+	}
+}
+
+func BenchmarkSplit(b *testing.B) {
+	r := getBenchRope()
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		r.Split(300000)
 	}
 }
 
 func BenchmarkInsert(b *testing.B) {
-	bytes := make([]byte, 409600)
-	n, err := rand.Read(bytes)
-	if n != len(bytes) || err != nil {
-		b.Fatalf("%d %v", n, err)
-	}
-	r := NewFromBytes(bytes)
+	r := getBenchRope()
 	b.ResetTimer()
-
 	for i := 0; i < b.N; i++ {
 		r.Insert(300000, []byte("foobar"))
 	}
 }
 
 func BenchmarkDelete(b *testing.B) {
-	bytes := make([]byte, 409600)
-	n, err := rand.Read(bytes)
-	if n != len(bytes) || err != nil {
-		b.Fatalf("%d %v", n, err)
-	}
-	r := NewFromBytes(bytes)
+	r := getBenchRope()
 	b.ResetTimer()
-
 	for i := 0; i < b.N; i++ {
 		r.Delete(300000, 400000)
 	}
 }
 
 func BenchmarkSub(b *testing.B) {
-	bytes := make([]byte, 409600)
-	n, err := rand.Read(bytes)
-	if n != len(bytes) || err != nil {
-		b.Fatalf("%d %v", n, err)
-	}
-	r := NewFromBytes(bytes)
+	r := getBenchRope()
 	b.ResetTimer()
-
 	for i := 0; i < b.N; i++ {
 		r.Sub(300000, 128)
 	}
 }
 
 func BenchmarkNewRuneReader(b *testing.B) {
-	bytes := make([]byte, 409600)
-	n, err := rand.Read(bytes)
-	if n != len(bytes) || err != nil {
-		b.Fatalf("%d %v", n, err)
-	}
-	r := NewFromBytes(bytes)
+	r := getBenchRope()
 	b.ResetTimer()
-
 	for i := 0; i < b.N; i++ {
 		r.NewRuneReader()
 	}
